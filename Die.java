6505bb669cb7cda209835d6b7fc0935e2e261cc2
SRajan13 <45398968+SRajan13@users.noreply.github.com>
2018-12-05 10:13:31 -0500

    partner A

---
+++ Die.java
@@ -0,0 +1,5 @@
+javac: invalid flag: C:\Users\10018739\Desktop\Github Practice\Dice-Project\Document1.txt
+Usage: javac <options> <source files>
+use -help for a list of possible options
+
+Tool completed with exit code 2
